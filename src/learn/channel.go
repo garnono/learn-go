7ba17a2948cbf3dc6aaae922dc7433ce9511b0f6
ouyanghua <ouyanghua@tal.com>
2021-06-16 18:43:34 +0800

    add doc

---
+++ src/learn/channel.go
@@ -0,0 +1,11 @@
+package learn
+
+func TestChannel()  {
+	PrintStart("channel")
+
+	// 特点：
+	// 声明
+	// 默认值
+	// 声明 & 赋值
+	// 注意：
+}
---
+++ src/learn/other.go
@@ -0,0 +1,21 @@
+package learn
+
+import (
+	"fmt"
+	"time"
+)
+
+func TestOther() {
+	PrintStart("other")
+
+	// 并发
+	go say("hello")
+	say("go")
+}
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
